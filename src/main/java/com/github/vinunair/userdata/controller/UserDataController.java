a650dbef585408d3eaa953fd9c5c0cfe178c2289
vinunair <mailvinu@gmail.com>
2016-04-12 14:00:58 +0800

    Added validation and user search form

--- src/main/java/com/github/vinunair/userdata/controller/UserDataController.java
+++ src/main/java/com/github/vinunair/userdata/controller/UserDataController.java
@@ -1,8 +1,12 @@
 package com.github.vinunair.userdata.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,9 +30,17 @@ public class UserDataController {
     }
 
     @RequestMapping(value = "user", method = RequestMethod.POST)
-    public String saveUser(User user) {
+    public String saveUser(@Valid User user,BindingResult bindingResult) {
+    	if(bindingResult.hasErrors())
+    		return "useradd";
+    	
         userDataService.saveUser(user);
         return "redirect:/user/add";
     }
-
+    
+    @RequestMapping("user/search/{name}")
+    public String searchUser(@PathVariable String name,Model model) {
+    	model.addAttribute("users", userDataService.searchUserByName(name));
+    	return "usersearch";
+    }
 }
--- src/main/java/com/github/vinunair/userdata/domain/User.java
+++ src/main/java/com/github/vinunair/userdata/domain/User.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 public class User {
@@ -11,8 +14,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 	
+	@NotEmpty(message="First Name cannot be empty")
 	private String firstname;
 	
+	@NotEmpty(message="Last Name cannot be empty")
 	private String lastname;
 
 	public Integer getId() {
